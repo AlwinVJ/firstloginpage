4868df803eead35a86f8b5bd7185e80b14d128e4
Alwin VJ <alwinvj5@gmail.com>
2022-12-27 15:40:48 +0530

    First commit on 27th December

---
+++ lib/widgets/login_field.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginField extends StatelessWidget {
+  const LoginField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(),
+    );
+  }
+}
